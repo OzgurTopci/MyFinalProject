04f88c314021a35d19aebe4852e1a299867f1d57
Ozgur <Ozgur@DESKTOP-8TFFC0A>
2023-12-20 21:40:47 +0300

    Add project files.

---
+++ Business/Abstract/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstarct
+{
+    public interface IProductService
+    {
+        List<Product>GetALL();
+        List<Product>GetAllByCategoryId(int id);
+        List<Product> GetAllByUnitPrice(decimal min, decimal max);
+
+    }
+}
