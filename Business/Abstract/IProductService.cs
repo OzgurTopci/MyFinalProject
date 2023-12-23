ab8368e29026d5d171a160a715ad00dad665c1a9
Ozgur <Ozgur@DESKTOP-8TFFC0A>
2023-12-23 14:27:12 +0300

    9.DersSonu

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -1,4 +1,5 @@
 ﻿using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,7 @@ namespace Business.Abstarct
         List<Product>GetALL();
         List<Product>GetAllByCategoryId(int id);
         List<Product> GetAllByUnitPrice(decimal min, decimal max);
+        List<ProductDetailDto> GetProductDetails();
 
     }
 }
